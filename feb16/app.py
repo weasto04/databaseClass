0e76993c95b557c94c0638ad9054d441a8f0de55
Whitley Easton <weasto04@lakers.mercyhurst.edu>
2026-02-19 14:29:44 +0000

    feb18commit

--- feb16/app.py
+++ feb16/app.py
@@ -2,12 +2,12 @@ import sqlite3
 import pandas as pd
 import gradio as gr
 
-def fetch_phillies():
-    conn = sqlite3.connect('../baseball.db')
-    cursor = conn.cursor()
-    query = """
-        WITH top_hitters AS (
-            SELECT people.nameFirst, people.nameLast
+
+conn = sqlite3.connect('../baseball.db')
+cursor = conn.cursor()
+query = """
+    WITH top_hitters AS (
+            SELECT people.nameFirst, people.nameLast, batting.playerID
             FROM batting
             INNER JOIN people ON batting.playerID = people.playerID
             WHERE teamID = 'PHI'
@@ -15,19 +15,18 @@ def fetch_phillies():
             ORDER BY sum(batting.HR) DESC
             LIMIT 10
         )
-        SELECT CONCAT(nameFirst, ' ', nameLast) as player
+        SELECT CONCAT(nameFirst, ' ', nameLast) as player, playerID
         FROM top_hitters
         ORDER BY nameLast ASC;
     """
-    cursor.execute(query)
-    records = cursor.fetchall()
-    conn.close()
-    players = []
-    for record in records:
-        players.append(record[0])
-    return players
+cursor.execute(query)
+records = cursor.fetchall()
+conn.close()
+#players = []
+#for record in records:
+#    players.append(record[0])
 
 with gr.Blocks() as iface:
-    playerID = gr.Dropdown(choices = fetch_phillies(), interactive = True)
+    playerID = gr.Dropdown(choices = records, interactive = True)
 
 iface.launch()
